27c688908586316907d16309fffaca8cf1a753cd
ssaxena <sankalpsaxena81@gmail.com>
2014-12-12 09:47:24 -0700

    adding test for adding negative numbers

--- test/com/travis/ci/demo/CalculatorTest.java
+++ test/com/travis/ci/demo/CalculatorTest.java
@@ -6,10 +6,17 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
-     @Test
+    @Test
     public void Add_shouldAdd_2_positiveNumbers() {
         Calculator calculator = new Calculator();
 
         assertEquals(3, calculator.add(1, 2));
+    }
+
+    @Test
+    public void Add_shouldAdd_2_negativeNumbers() {
+        Calculator calculator = new Calculator();
+
+        assertEquals(-3, calculator.add(-1, -2));
     }
 }
