15906f0c26b867a0d4eb3f169562234a47948030
ssaxena <sankalpsaxena81@gmail.com>
2014-12-12 09:25:16 -0700

    first checkin

---
+++ src/com/travis/ci/demo/Calculator.java
@@ -0,0 +1,8 @@
+package com.travis.ci.demo;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ test/com/travis/ci/demo/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.travis.ci.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+     @Test
+    public void Add_shouldAdd_2_positiveNumbers() {
+        Calculator calculator = new Calculator();
+
+        assertEquals(3, calculator.add(1, 2));
+    }
+}
